e7fe90889db2d4cc71ef07e64d6bbb39629416d4
Victor Hugo <victorhugo@Victors-MacBook-Pro.local>
2020-07-14 21:59:17 -0300

    loading django

--- my_app/models.py
+++ my_app/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 #Django ORM - Object Relational Mapper
 class Post(models.Model):           #representa uma tabela no bd
-    title       = models.CharField(max_length=255)
-    content     = models.TextField()
+    title       = models.CharField(max_length=255, verbose_name='Título')
+    content     = models.TextField(verbose_name='Corpo do artigo')
+    user        = models.ForeignKey(User,on_delete=models.PROTECT, verbose_name='Autor')
+
+    class Meta:
+        verbose_name = 'Artigo'
+        #verbose_name_plural = 'Artigos'
+
+    def __str__(self):
+       return self.title  
     
     
